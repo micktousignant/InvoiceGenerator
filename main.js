2d1c0025eee55fc591f6b9ab4ed33cbe3c7dd15f
Michael <micktousignant@gmail.com>
2019-04-25 22:22:56 -0400

    Calculate sous total

--- main.js
+++ main.js
@@ -8,6 +8,17 @@ function ChangeInvoiceDate(event) {
   InvoiceDate.innerHTML = event.target.DateText.value;
 }
 
+function AddItemToInvoice(event) {
+  event.preventDefault();
+  let ItemTotalPrice = parseFloat(event.target.ItemPrice.value) * parseFloat(event.target.ItemQantity.value);
+  console.log(InvoiceItemRows.innerHTML += "<tr class=\"item\"><td>" + event.target.ItemName.value +
+                                           "</td><td>" + event.target.ItemType.value +
+                                           "</td><td>" + event.target.ItemPrice.value + "$" +
+                                           "</td><td>" + event.target.ItemQantity.value +
+                                           "</td><td>" + (ItemTotalPrice).toFixed(2) + "$</td></tr>");
+  SousTotalText.innerHTML = (parseFloat(SousTotalText.innerHTML) + ItemTotalPrice).toFixed(2);
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   let testerNumber = document.querySelector("#InvoiceNumberForm");
   testerNumber.addEventListener("submit", function(event) {
@@ -16,5 +27,9 @@ document.addEventListener("DOMContentLoaded", function() {
   let testerDate = document.querySelector("#InvoiceDateForm");
   testerDate.addEventListener("submit", function(event) {
     ChangeInvoiceDate(event);
+  });
+  let testerAddItem = document.querySelector("#AddItemForm");
+  testerAddItem.addEventListener("submit", function(event) {
+    AddItemToInvoice(event);
   });
 });
