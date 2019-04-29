dddc529ef1c46c44173024c6f429e20328aa077b
Michael <micktousignant@gmail.com>
2019-04-29 12:20:29 -0400

    Add Change Description and Change Client features

--- main.js
+++ main.js
@@ -8,15 +8,40 @@ function ChangeInvoiceDate(event) {
   InvoiceDate.innerHTML = event.target.DateText.value;
 }
 
+function ChangeInvoiceDescription(event) {
+  event.preventDefault();
+  InvoiceDescription.innerHTML = event.target.DescriptionText.value;
+}
+
 function AddItemToInvoice(event) {
   event.preventDefault();
   let ItemTotalPrice = parseFloat(event.target.ItemPrice.value) * parseFloat(event.target.ItemQantity.value);
+  let SousTotal = parseFloat(SousTotalText.innerHTML);
+  let TPSamount = 0.0;
+  let TVQamount = 0.0;
+  let Shipping = parseFloat(ShippingText.innerHTML);
+  let GrandTotal = 0.0;
   console.log(InvoiceItemRows.innerHTML += "<tr class=\"item\"><td>" + event.target.ItemName.value +
                                            "</td><td>" + event.target.ItemType.value +
                                            "</td><td>" + event.target.ItemPrice.value + "$" +
                                            "</td><td>" + event.target.ItemQantity.value +
                                            "</td><td>" + (ItemTotalPrice).toFixed(2) + "$</td></tr>");
-  SousTotalText.innerHTML = (parseFloat(SousTotalText.innerHTML) + ItemTotalPrice).toFixed(2);
+  SousTotal += ItemTotalPrice;
+  SousTotalText.innerHTML = SousTotal.toFixed(2) + "$";
+  TPSamount = SousTotal * 0.05;
+  TPSText.innerHTML = TPSamount.toFixed(2) + "$";
+  TVQamount = SousTotal * 0.09975;
+  TVQText.innerHTML = TVQamount.toFixed(2) + "$";
+  GrandTotal = SousTotal + TPSamount + TVQamount + Shipping;
+  GrandTotalText.innerHTML = GrandTotal.toFixed(2) + "$";
+}
+
+function ChangeInvoiceClient(event) {
+  event.preventDefault();
+  ClientInfo.innerHTML = "<b>Facturé à</b><br>" + event.target.ClientName.value +
+                         "<br>" + event.target.ClientAddress.value +
+                         "<br>" + event.target.ClientCity.value + ", " + event.target.ClientProv.value +
+                         "<br>" + event.target.ClientZip.value + "<br>"
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -28,8 +53,16 @@ document.addEventListener("DOMContentLoaded", function() {
   testerDate.addEventListener("submit", function(event) {
     ChangeInvoiceDate(event);
   });
+  let testerDescription = document.querySelector("#InvoiceDescriptionForm");
+  testerDescription.addEventListener("submit", function(event) {
+    ChangeInvoiceDescription(event);
+  });
   let testerAddItem = document.querySelector("#AddItemForm");
   testerAddItem.addEventListener("submit", function(event) {
     AddItemToInvoice(event);
+  });
+  let testerChangeClient = document.querySelector("#ChangeClientForm");
+  testerChangeClient.addEventListener("submit", function(event) {
+    ChangeInvoiceClient(event);
   });
 });
